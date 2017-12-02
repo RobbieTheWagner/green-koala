60ee9edff59f5057b52be32604ddc39bd2b0b551
Robert Wagner <rwwagner90@gmail.com>
2017-12-02 14:34:40 -0500

    Start on account form

---
+++ components/AccountInfoForm/AccountInfoForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form-schema :schema="schema" v-model="model" @submit="submit">
+    <button type="submit">Submit</button>
+  </form-schema>
+</template>
+
+<script>
+  import FormSchema from 'vue-json-schema';
+  import schema from './schema.json';
+
+  export default {
+    data: () => ({
+      schema,
+      model: {}
+    }),
+    methods: {
+      submit(e) {
+        // this.model contains the valid data according your JSON Schema.
+        // You can submit your model to the server here
+      }
+    },
+    components: { FormSchema }
+  };
+</script>
+
+<style>
+  input {
+    display: block;
+  }
+</style>
---
+++ pages/account.vue
@@ -0,0 +1,13 @@
+<template>
+  <account-info-form/>
+</template>
+
+<script>
+  import AccountInfoForm from '~/components/AccountInfoForm/AccountInfoForm.vue';
+
+  export default {
+    components: {
+      AccountInfoForm
+    }
+  };
+</script>
